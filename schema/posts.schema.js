71abcef0defde5ade0de17650280f8343709e410
harshil <maniarharshil1000@gmail.com>
2020-06-22 09:24:23 +0530

    own method for creating posts

--- schema/posts.schema.js
+++ schema/posts.schema.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+
 const PostSchema = new mongoose.Schema({
     title:  String,
     content: String ,
     votes: { type: Number, default: 0}
 });
 
-module.exports = PostSchema
+module.exports = PostSchema;
--- services/dataFetch.service.js
+++ services/dataFetch.service.js
@@ -3,6 +3,12 @@ const MongooseAdapter = require("moleculer-db-adapter-mongo");
 const PostSchema = require('../schema/posts.schema');
 const mongoose = require('mongoose')
 require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL,{
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+    useFindAndModify : false,  
+    socketTimeoutMS: 45000,
+    keepAlive: true});
 
 module.exports = {
     name : "fetch-posts",
@@ -26,18 +32,15 @@ module.exports = {
     },
 
     methods : {
-        createPost(content, votes = 0 ){
-            const PostModel = mongoose.model("Post", PostSchema);
-            const newPost = new PostModel({content : content, votes : votes ||0});
+        createPost(content, votes){
+            const model = mongoose.model('post',PostSchema)
+            const newPost = new model({content : content, votes : 999});
             console.log(newPost);
             console.log(mongoose.connection.readyState);
-            newPost.markModified('votes');
             newPost.save( function (err,res){
                 if(err){
-                    console.log(err,1);
-                    return 
+                    throw err 
                 }
-                console.log(2)
                 return res;
             });               
         }
