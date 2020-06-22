f1ed73b470e21905a0edb26828e65329e5fe43e5
harshil <maniarharshil1000@gmail.com>
2020-06-22 10:13:34 +0530

    override default mongoose method

--- services/dataFetch.service.js
+++ services/dataFetch.service.js
@@ -2,7 +2,10 @@ const DbService = require("moleculer-db");
 const MongooseAdapter = require("moleculer-db-adapter-mongo");
 const PostSchema = require('../schema/posts.schema');
 const mongoose = require('mongoose')
+const model = mongoose.model('post',PostSchema)
+
 require('dotenv').config();
+
 mongoose.connect(process.env.MONGO_URL,{
     useUnifiedTopology: true, 
     useNewUrlParser: true,
@@ -18,31 +21,57 @@ module.exports = {
     model : PostSchema,
 
     actions : {
-
+        create : false,
+        list : false,
         createPost: {
             rest : {
                 method : "POST",
-                path : "/create",
+                path : "/",
             },
 
             async handler(ctx){
-                return await this.createPost(ctx.params.content, ctx.params.votes)
+                let res = await this.createPost(ctx.params.content, ctx.params.votes)
+                return res;
+            }
+        },
+
+        findPost : {
+            rest : {
+                method : "GET",
+                path : "/",
+            },
+
+            async handler(ctx){
+                let res = await this.findPosts();
+                return res;
             }
         }
     },
 
     methods : {
         createPost(content, votes){
-            const model = mongoose.model('post',PostSchema)
-            const newPost = new model({content : content, votes : 999});
-            console.log(newPost);
-            console.log(mongoose.connection.readyState);
-            newPost.save( function (err,res){
+            return new Promise((resolve,reject)=>{
+                const newPost = new model({content : content, votes : votes ||0});
+                console.log(newPost);
+                console.log(mongoose.connection.readyState);
+                newPost.save( function (err,res){
+                    if(err){
+                        reject(err) 
+                    }
+                    resolve(res);
+                });   
+            })            
+        },
+
+        findPosts(){
+           return new Promise((resolve,reject)=>{
+            let posts = model.find({},(err,res)=>{
                 if(err){
-                    throw err 
+                    reject(err)
                 }
-                return res;
-            });               
+                resolve(posts);
+            })
+           })
         }
     }
 }
